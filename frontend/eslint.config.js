ef307e504afc4840507202f8705f1d2837fe207b
Shahzeb Parkar <shahzebparkar@gmail.com>
2026-05-16 17:51:43 +0530

    feat: update frontend to React 19. - enhance EmployeesPage with debounced search - improve Dockerfile setup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react'; // <-- Import the React plugin
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
@@ -8,14 +9,24 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'], 
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ]);
